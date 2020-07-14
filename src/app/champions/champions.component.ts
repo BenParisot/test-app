0c3d9ddb049b18349898d545b6ecc72fc105fc4f
Ben Parisot <me@benparisot.com>
2020-07-13 19:59:45 -0700

    adds data binding for form module in champion component

--- src/app/champions/champions.component.ts
+++ src/app/champions/champions.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Champion } from '../champion'
 
 @Component({
   selector: 'app-champions',
   templateUrl: './champions.component.html',
-  styleUrls: ['./champions.component.sass']
+  styleUrls: ['./champions.component.scss']
 })
 export class ChampionsComponent implements OnInit {
-
+  champion: Champion = {
+    id: 1,
+    name: 'Spider-Man'
+  };
   constructor() { }
 
   ngOnInit(): void {
